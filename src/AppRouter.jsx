d497afe4593d359973d0d18e317331d9c56953c9
francamateus93 <francamateus@gmail.com>
2025-01-29 11:39:15 +0100

    fix: Update the ShipCard component and configure the Routing

--- src/AppRouter.jsx
+++ src/AppRouter.jsx
@@ -11,7 +11,7 @@ function App() {
       <Routes>
         <Route path="/" element={<HomePage />} />
         <Route path="/starships" element={<StarshipsPage />} />
-        <Route path="/starships/:id" element={<ShipDetails />} />
+        <Route path="/starship/:id" element={<ShipDetails />} />
         <Route path="*" element={<NotFound />} />
       </Routes>
     </BrowserRouter>
--- src/pages/ShipDetails.jsx
+++ src/pages/ShipDetails.jsx
@@ -1,23 +1,16 @@
 import React from "react";
-import { Link } from "react-router-dom";
 
-function NotFound() {
+function ShipDetails() {
   return (
     <div className="flex gap-8 items-center justify-center h-screen">
       <div className="flex flex-col p-4 mr-8">
-        <h1 className="text-6xl font-extrabold mb-6">404 - Page Not Found</h1>
+        <h1 className="text-6xl font-extrabold mb-6">Ship Details</h1>
         <p className="text-xl text-center text-gray-400 mb-4">
-          It appears that the page you are looking for does not exist.
+          Ship Details,Ship Details,Ship Details,Ship Details.
         </p>
-        <Link
-          to="/"
-          className="px-8 py-4 shadow-lg mt-4 text-center rounded-lg text-white bg-cyan-600 hover:text-white hover:bg-cyan-700 transition"
-        >
-          Back to Home
-        </Link>
       </div>
     </div>
   );
 }
 
-export default NotFound;
+export default ShipDetails;
