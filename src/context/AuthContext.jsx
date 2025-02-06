4b5853a63eb6fd24805864fa9b9f2aa3525f02c4
francamateus93 <francamateus@gmail.com>
2025-02-06 13:01:12 +0100

    refactor(authContext): implement useReducer

--- src/context/AuthContext.jsx
+++ src/context/AuthContext.jsx
@@ -1,5 +1,5 @@
 import { auth } from "../firebase/firebase";
-import { createContext, useContext, useEffect, useState } from "react";
+import { createContext, useContext, useEffect, useReducer } from "react";
 import {
   createUserWithEmailAndPassword,
   signInWithEmailAndPassword,
@@ -7,29 +7,80 @@ import {
   signOut,
 } from "firebase/auth";
 
+const initialState = {
+  user: null,
+  loading: true,
+};
+
+const authReducer = (state, action) => {
+  switch (action.type) {
+    case "login":
+      return {
+        ...state,
+        user: action.payload,
+        loading: false,
+      };
+    case "logout":
+      return {
+        ...state,
+        user: anull,
+        loading: false,
+      };
+    case "setUser":
+      return {
+        ...state,
+        user: action.payload,
+        loading: false,
+      };
+    case "loading":
+      return {
+        ...state,
+        loading: true,
+      };
+    default:
+      return state;
+  }
+};
+
 const AuthContext = createContext();
 
 export const AuthProvider = ({ children }) => {
-  const [user, setUser] = useState(null);
-  const [loading, setLoading] = useState(true);
+  const [user, dispatch] = useReducer(authReducer, initialState);
 
   useEffect(() => {
     const unsubscribe = onAuthStateChanged(auth, (user) => {
-      setUser(user);
-      setLoading(false);
+      dispatch({ type: "setUser", payload: user });
     });
 
     return unsubscribe;
   }, []);
 
-  const login = (email, password) =>
-    signInWithEmailAndPassword(auth, email, password);
-  const signup = (email, password) =>
-    createUserWithEmailAndPassword(auth, email, password);
-  const logout = () => signOut(auth);
+  const login = async (email, password) => {
+    dispatch({ type: "loading" });
+    const response = await signInWithEmailAndPassword(auth, email, password);
+    dispatch({ type: "login", payload: response.user });
+  };
+
+  const signup = async (email, password) => {
+    dispatch({ type: "loading" });
+    const response = await createUserWithEmailAndPassword(
+      auth,
+      email,
+      password
+    );
+    dispatch({ type: "login", payload: response.user });
+  };
+
+  const logout = async () => {
+    dispatch({ type: "loading" });
+    await signOut(auth);
+    dispatch({ type: "logout" });
+  };
 
   return (
-    <AuthContext.Provider value={{ user, login, signup, logout, loading }}>
+    <AuthContext.Provider
+      value={{ user: user.state, login, signup, logout, loading: user.state }}
+    >
       {children}
     </AuthContext.Provider>
   );
